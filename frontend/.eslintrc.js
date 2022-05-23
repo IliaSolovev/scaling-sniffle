22855ec055182f0d1e661f12a4b44c03deac0a70
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2022-05-24 00:46:35 +0300

    Create main files and settings for frontend and smart-contract folders (#1)
    * init: smart-contract
    
    * init: frontend
    
    * init: added eslint
    
    * init: added wallet
    
    * init/frontend: added alias to components
    
    * feature: deleted disconnect button
    
    * init/frontend: add airbnb eslint config

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'import/no-unresolved': 0, // because ts already throw error if we can`t resolve path
+    'react/jsx-filename-extension': ['error', { extensions: ['.ts', '.tsx'] }],
+  },
+};
---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Wallet } from 'components';
+
+function App() {
+  return (
+    <Wallet>
+      <div className="App">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </div>
+    </Wallet>
+  );
+}
+
+export default App;
